878ac4823b916f8a39fe9755181c5d0a9481fe8a
thudzj <dengzhijiethu@gmail.com>
2022-01-27 22:49:33 +0800

    Add nystrom method to tackle ntk

--- tmp/plot.py
+++ tmp/plot.py
@@ -90,8 +90,8 @@ def draw_eigenvalues(eigenval_gd, eigenval_nystrom, eigenval_our):
 	axins = ax.inset_axes([0.28, 0.28, 0.7, 0.7]) # zoom = 6
 	'''
 	axins = ax
-	axins.plot(np.array(list(range(10))) + 1, eigenval_gd[:10] + 0.002, marker='o', markersize=5, label='Ground truth')
-	# axins.plot(np.array(list(range(10))) + 0.05, eigenval_nystrom[:10] + 0.002, '--*', markersize=6)
+	axins.plot(np.array(list(range(10))) + 1, eigenval_gd[:10], marker='o', markersize=5, label='Ground truth')
+	# axins.plot(np.array(list(range(10))) + 0.05, eigenval_nystrom[:10], '--*', markersize=6)
 	axins.plot(np.array(list(range(10))) + 1, eigenval_our[:10], ':v', markersize=6, label='Our method')
 	# sub region of the original image
 	x1, x2, y1, y2 = 0, 10.5, 0.008, 0.33
