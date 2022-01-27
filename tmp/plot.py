eb6332bbb2ed0738280c78eae41d0cebe08e1023
thudzj <dengzhijiethu@gmail.com>
2022-01-27 22:22:28 +0800

    Add nystrom method to tackle ntk

--- tmp/plot.py
+++ tmp/plot.py
@@ -55,11 +55,15 @@ eigenval_our = npzfile['our']
 
 kernels = list(npzfile2['k'])
 
+# kernels[1] = kernels[0] - kernels[1]
+# kernels[2] = kernels[0] - kernels[2]
+# kernels[3] = kernels[0] - kernels[3]
+
 def draw_eigenvalues(eigenval_gd, eigenval_nystrom, eigenval_our):
 	from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
 	from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 
-	fig = plt.figure(figsize=(7, 5))
+	fig = plt.figure(figsize=(5, 4.5))
 	ax = fig.add_subplot(111)
 	ax.tick_params(axis='y', which='major', labelsize=12)
 	ax.tick_params(axis='y', which='minor', labelsize=12)
@@ -68,8 +72,9 @@ def draw_eigenvalues(eigenval_gd, eigenval_nystrom, eigenval_our):
 
 	sns.color_palette()
 
+	'''
 	ax.plot(list(range(len(eigenval_gd))), eigenval_gd, alpha=1, marker='o', markersize=2, label='Ground truth')
-	ax.plot(list(range(len(eigenval_nystrom))), eigenval_nystrom, '--*', markersize=5, label='The Nyström method')
+	# ax.plot(list(range(len(eigenval_nystrom))), eigenval_nystrom, '--*', markersize=5, label='The Nyström method')
 	ax.plot(list(range(len(eigenval_our))), eigenval_our, ':v', markersize=3, label='Our')
 	ax.set_yscale('log')
 	ax.set_xlabel('$i$-th eigenvalue', fontsize=16)
@@ -81,36 +86,42 @@ def draw_eigenvalues(eigenval_gd, eigenval_nystrom, eigenval_our):
 	ax.spines['left'].set_color('gray')
 	ax.set_axisbelow(True)
 
+
 	axins = ax.inset_axes([0.28, 0.28, 0.7, 0.7]) # zoom = 6
-	axins.plot(np.array(list(range(10))) + 0.05, eigenval_gd[:10] + 0.002, marker='o', markersize=5)
-	axins.plot(np.array(list(range(10))) + 0.05, eigenval_nystrom[:10] + 0.002, '--*', markersize=6)
-	axins.plot(np.array(list(range(10))) + 0.05, eigenval_our[:10] + 0.002, ':v', markersize=6)
+	'''
+	axins = ax
+	axins.plot(np.array(list(range(10))) + 1, eigenval_gd[:10], marker='o', markersize=5, label='Ground truth')
+	# axins.plot(np.array(list(range(10))) + 0.05, eigenval_nystrom[:10], '--*', markersize=6)
+	axins.plot(np.array(list(range(10))) + 1, eigenval_our[:10], ':v', markersize=6, label='Our method')
 	# sub region of the original image
-	x1, x2, y1, y2 = 0, 9.5, 0.008, 0.33
+	x1, x2, y1, y2 = 0, 10.5, 0.008, 0.33
 	# axins.set_yscale('log')
 	axins.set_xlim(x1, x2)
 	axins.set_ylim(y1, y2)
-	axins.tick_params(axis='x', labelsize= 8)
-	axins.tick_params(axis='y', labelsize= 8)
+	# axins.tick_params(axis='x', labelsize= 8)
+	# axins.tick_params(axis='y', labelsize= 8)
 	axins.set_xticks(range(10))
 	axins.set_xticks(range(10), minor=True)
-	axins.set_yticks([0.01, 0.1, 0.2, 0.3])
-	axins.set_yticks([0.01, 0.1, 0.2, 0.3], minor=True)
+	axins.set_yticks([0.0, 0.1, 0.2, 0.3])
+	axins.set_yticks([0.0, 0.1, 0.2, 0.3], minor=True)
 
-	# draw a bbox of the region of the inset axes in the parent axes and
-	# connecting lines between the bbox and the inset axes area
-	mark_inset(ax, axins, loc1=2, loc2=3, fc="none", ec="0.5")
+	# mark_inset(ax, axins, loc1=2, loc2=3, fc="none", ec="0.5")
 
-	ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.14),
-          ncol=3, fancybox=True, shadow=True, fontsize=13)
+	# ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.14),
+    #       ncol=3, fancybox=True, shadow=True, fontsize=13)
+	ax.set_xlabel('$i$-th eigenvalue', fontsize=16)
+	ax.legend(loc='best')
 
 	fig.tight_layout()
 	fig.savefig(os.path.join('eigenvalues_{}.pdf'.format(nef_k)), format='pdf', dpi=1000, bbox_inches='tight')
 
 def draw_kernel(kernels):
-	labels = ['Ground truth', 'The Nyström method ($k=10$)', 'Our ($k=10$)', 'Random feature approach ($S=10$)', ]
+	labels = ['Ground truth', 'The Nyström method ($k=10$)', 'Our method ($k=10$)', 'Random feature approach ($S=10$)', ]
 	ma = 1
 	mi = -1
+
+	labels.pop(1)
+	kernels.pop(1)
 
 	fig = plt.figure(figsize=(5*len(kernels), 5))
 	for i, (k, l) in enumerate(zip(kernels, labels)):
